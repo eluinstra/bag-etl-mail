3ccb82d3fd1f2dc594b901239b1adadecebb34f7
eluinstra <edwin.luinstra@clockwork.nl>
2013-10-08 23:24:44 +0200

    added main method

--- src/main/java/nl/ordina/bag/etl/mail/scheduler/ProcessMailJob.java
+++ src/main/java/nl/ordina/bag/etl/mail/scheduler/ProcessMailJob.java
@@ -2,6 +2,7 @@ package nl.ordina.bag.etl.mail.scheduler;
 
 import nl.ordina.bag.etl.loader.MutatiesLoader;
 import nl.ordina.bag.etl.mail.loader.MailProcessor;
+import nl.ordina.bag.etl.util.BeanLocator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -29,5 +30,16 @@ public class ProcessMailJob extends JobScheduler
 	public void setMutatiesLoader(MutatiesLoader mutatiesLoader)
 	{
 		this.mutatiesLoader = mutatiesLoader;
+	}
+	
+	public static void main(String[] args)
+	{
+		BeanLocator beanLocator = BeanLocator.getInstance("nl/ordina/bag/etl/mail/applicationConfig.xml","nl/ordina/bag/etl/applicationContext.xml","nl/ordina/bag/etl/mail/mutaties.xml");
+		MailProcessor mutatiesFileLoader = (MailProcessor)beanLocator.get("mutatiesFileMailLoader");
+		MutatiesLoader mutatiesLoader = (MutatiesLoader)beanLocator.get("mutatiesLoader");
+		ProcessMailJob job = new ProcessMailJob();
+		job.setMutatiesFileLoader(mutatiesFileLoader);
+		job.setMutatiesLoader(mutatiesLoader);
+		job.execute();
 	}
 }
